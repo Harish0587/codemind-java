ebf04102edf65a9d95f3130cc047ce214daf818a
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-08-19 11:57:07 +0530

    import java.util.*; public class main{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a,b;         a= sc.nextInt();         b= sc.nextInt();         int n=a%b;         System.out.println(n);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a= sc.nextInt();
+        b= sc.nextInt();
+        int n=a%b;
+        System.out.println(n);
+    }
+}
