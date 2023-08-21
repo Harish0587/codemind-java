06b3c3e9f2413745dad521107649b80f8fb7c0d4
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-08-21 22:29:06 +0530

    import java.util.*; public class main{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int nt,ns,nb;         nt=sc.nextInt();         ns=sc.nextInt();         nb=sc.nextInt();         System.out.println(2*nt*ns*nb*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int nt,ns,nb;
+        nt=sc.nextInt();
+        ns=sc.nextInt();
+        nb=sc.nextInt();
+        System.out.println(2*nt*ns*nb*512);
+    }
+}
