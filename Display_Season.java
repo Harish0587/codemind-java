4dab77f4becebcd771aad0616f661d452f9d3c82
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-08-26 10:00:51 +0530

    import java.util.*; public class main{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         if(n==4 || n==5 || n==6)         {             System.out.println("Summer");         }         else if(n==7 || n==8 || n==9 || n==10)         {             System.out.println("Rainy");         }         else if(n==11 || n==12 || n==1)         {             System.out.println("Winter");         }         else if(n==2 || n==3)         {             System.out.println("Spring");         }         else{             System.out.println("-1");         }     } }

---
+++ Display_Season.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==4 || n==5 || n==6)
+        {
+            System.out.println("Summer");
+        }
+        else if(n==7 || n==8 || n==9 || n==10)
+        {
+            System.out.println("Rainy");
+        }
+        else if(n==11 || n==12 || n==1)
+        {
+            System.out.println("Winter");
+        }
+        else if(n==2 || n==3)
+        {
+            System.out.println("Spring");
+        }
+        else{
+            System.out.println("-1");
+        }
+    }
+}
