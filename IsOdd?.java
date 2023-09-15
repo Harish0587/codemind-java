989b35ab5dc444383b3e3efc166b271a1a980c48
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-09-16 00:41:39 +0530

    import java.util.*; public class main{     static int odd(int n)     {         if(n%2!=0)         {             return 2;         }         else         {             return 1;         }     }     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         System.out.println(odd(n));     } }

---
+++ IsOdd?.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class main{
+    static int odd(int n)
+    {
+        if(n%2!=0)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(odd(n));
+    }
+}
