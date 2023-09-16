def251430a60cff3c79b3bac01fde1a30467d33c
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-09-16 19:46:06 +0530

    import java.util.*; public class main{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         int sum=(n*(n+1))/2;         System.out.println(sum);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=(n*(n+1))/2;
+        System.out.println(sum);
+    }
+}
