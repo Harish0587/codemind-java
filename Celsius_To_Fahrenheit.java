b6d65346c42b6cc274ae0db0a597f47aada68968
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-09-16 15:52:42 +0530

    import java.util.*; public class main{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int c=sc.nextInt();         double f=(c*1.8)+32;         System.out.printf("%.2f",f);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int c=sc.nextInt();
+        double f=(c*1.8)+32;
+        System.out.printf("%.2f",f);
+    }
+}
