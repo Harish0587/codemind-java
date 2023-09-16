74c24f1a734addb98703ff78ebe32b0549836f68
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2023-09-16 16:03:39 +0530

    import java.util.*; public class main{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int p,t,r;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         int si=(p*t*r)/100;         System.out.println(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int p,t,r;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        int si=(p*t*r)/100;
+        System.out.println(si);
+    }
+}
